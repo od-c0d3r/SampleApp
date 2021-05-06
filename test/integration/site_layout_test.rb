276bbebdb035cff1f3ce2e5ee3430f91cb990a5d
rashad <omar_rashad@aol.com>
2021-05-06 11:37:15 +0200

    Defining sign up utilty

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
   end
 end
