656b64cd2002dc2f8fd2fb6acb5497edd8f63fb0
rashad <omar_rashad@aol.com>
2021-05-06 10:08:17 +0200

    Adding links and corsponding tests

---
+++ test/integration/full_title_helper_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class FullTitleHelperTest < ActionDispatch::IntegrationTest
+  test "full title helper" do
+    assert_equal full_title,         'Ruby on Rails Tutorial Sample App'
+    assert_equal full_title("Help"), 'Help | Ruby on Rails Tutorial Sample App'
+  end
+end
