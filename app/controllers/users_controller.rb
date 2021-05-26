d90f21118aaaf122f1e6f3e8c0e1a4070846533e
od-c0d3r <omar_rashad@aol.com>
2021-05-26 14:04:15 +0200

    Creating New user Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
-end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # if succeed
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+end 
