17c97753665cda26d24283c65c85ca9627bcb615
od-c0d3r <omar_rashad@aol.com>
2021-06-08 11:28:16 +0200

    Adding Protrciton level to private pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -20,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "User updated!"
       redirect_to @user
@@ -37,6 +38,18 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless @user == current_user
     end
 
 end 
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,9 +3,11 @@ require "test_helper"
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:omar)
+    @other_user = users(:rashad)
   end
 
   test "invalid user update" do
+    log_in_as(@user)
     get edit_user_path(@user)
     patch user_path(@user), params: { user: { name: "invalid!@#",
                                     email:"invalid!@#invalid!@#",
@@ -15,6 +17,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "valid user update" do
+    log_in_as(@user)
     get edit_user_path(@user)
     name = "foo"
     email = "fo@bar.com"
@@ -27,5 +30,31 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when not correct user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert_redirected_to root_path
+  end
+
+  test "should redirect update when not correct user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_redirected_to root_path
   end
 end
